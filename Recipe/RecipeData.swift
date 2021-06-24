97cd25eb390c44c21b903bb0ff806c7722379fc8
kuehar <green95251@gmail.com>
2021-06-25 01:55:43 +0900

    added code request to json and recieve response from json

--- Recipe/RecipeData.swift
+++ Recipe/RecipeData.swift
@@ -11,7 +11,41 @@ import Foundation
 // レシピ検索用クラス
 class RecipeData: ObservableObject{
     
+    struct ResultJson:Codable {
+        struct item:Codable {
+            let name: String?
+            let url: URL?
+            let image:URL?
+        }
+        let item: [Item]?
+    }
+    
     func searchRecipe(keyword:String){
         print(keyword)
+        
+        guard let keyword_encode = keyword.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else {
+            return
+        }
+        
+        guard let req_url = URL(string: "https://sysbird.jp/toriko/api/?apikey=guest&format=json&keyword=\(keyword_encode)&max=10&order=r") else {
+            return
+        }
+        print(req_url)
+        
+        let req = URLRequest(url:req_url)
+        let session = URLSession(configuration: .default,delegate: nil,delegateQueue: OperationQueue.main)
+        let task = session.dataTask(with:req,completionHandler: {
+            (data,response,error) in
+            session.finishTasksAndInvalidate()
+            do{
+                let decoder = JSONDecoder()
+                let json = try decoder.decode(ResultJson.self, from: data!)
+                
+                print(json)
+            }catch{
+                print("エラーが出ました")
+            }
+        })
+        task.resume()
     }
 }
