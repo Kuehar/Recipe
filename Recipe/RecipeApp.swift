f0e7aa83773da37e4c5e864fe0e433988fa99549
kuehar <green95251@gmail.com>
2021-06-24 12:51:29 +0900

    Initial Commit

---
+++ Recipe/RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeApp.swift
+//  Recipe
+//
+//  Created by kuehar on 2021/06/24.
+//
+
+import SwiftUI
+
+@main
+struct RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
